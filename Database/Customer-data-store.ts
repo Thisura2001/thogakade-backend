460bccf0ecacc8620f35dea3829aeea1a940b0aa
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-01-25 21:51:24 +0530

    customer and item data-store done

---
+++ Database/Customer-data-store.ts
@@ -0,0 +1,59 @@
+import {PrismaClient} from "@prisma/client";
+import Customer from "../Model/Customer";
+
+const prisma =new PrismaClient();
+
+export async function addCustomer(cus:Customer){
+    try {
+        const customer = await prisma.customers.create({
+            data:{
+                id:cus.id,
+                name:cus.name,
+                nic:cus.nic,
+                email:cus.email,
+                phone:cus.phone
+            }
+        })
+        console.log("Customer added ",customer)
+        return customer;
+    }catch (err){
+        console.log("Error adding customers ",err)
+    }
+}
+export async function deleteCustomer(id:number){
+    try {
+        const deletedCustomer = await prisma.customers.delete({
+            where:{
+                id:id
+            }
+        })
+        console.log("Customer deleted!",deletedCustomer);
+        return deletedCustomer;
+    }catch (err){
+        console.log("Error deleting customer ",err);
+    }
+}
+export async function updateCustomer(id:number,cus:Customer){
+    try {
+        const updatedCustomer = await prisma.customers.update({
+            where:{id:id},
+            data:{
+                name:cus.name,
+                nic:cus.nic,
+                email:cus.email,
+                phone:cus.phone
+            }
+        })
+        console.log("Customer updated ",updatedCustomer);
+        return updatedCustomer;
+    }catch (err){
+        console.log("Error updating customer ",err);
+    }
+}
+export async function getAllCustomers(){
+    try {
+        return await prisma.customers.findMany()
+    }catch (err){
+        console.log("Error getting customers ",err)
+    }
+}
---
+++ Database/Item-data-Store.ts
@@ -0,0 +1,55 @@
+import {PrismaClient} from "@prisma/client";
+import Item from "../Model/Item";
+
+const prisma = new PrismaClient();
+
+export async function addItems(i:Item){
+    try{
+        const addedItem = await prisma.items.create({
+            data:{
+                id:i.id,
+                name:i.name,
+                price:i.price,
+                quantity:i.quantity
+            }
+        })
+        console.log("Item added ",addedItem)
+        return addedItem;
+    }catch (err){
+        console.log("Error adding Items ",err)
+    }
+}
+export async function deleteItem(id:number){
+    try {
+        const deleteItem = await prisma.items.delete({
+            where:{
+                id:id
+            }
+        })
+        console.log("Item deleted ",deleteItem);
+        return deleteItem
+    }catch (err){
+        console.log("Error deleting item ",err)
+    }
+}
+export async function updateItems(id:number,i:Item){
+    try {
+        const updatedItem = await prisma.items.update({
+            where:{id:id},
+            data:{
+                name:i.name,
+                price:i.price,
+                quantity:i.quantity
+            }
+        })
+    }catch (err){
+        console.log("Error updating items ",err)
+    }
+}
+export async function getAllItems(){
+    try {
+       return await prisma.items.findMany();
+    }catch (err){
+        console.log("Error getting items ",err)
+    }
+}
